5cad38ede5bf8a49daf2735c20a5c73ec6587fd2
Ahmet U <cyberpunk2099@outlook.com>
2018-10-14 02:29:15 +0300

    Exercise-002 added

---
+++ src/Exercise-002/MaximumSubarray/MaximumSubarray/Calc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumSubarray
+{
+    public static class Calc
+    {
+        public static string GetMaximumSubarray(string arrayStr)
+        {
+            if (string.IsNullOrWhiteSpace(arrayStr)) throw new ArgumentException(nameof(arrayStr));
+            var array = GetMaximumSubarray(arrayStr.Trim()
+                .Split(new char[] { ';', ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => int.Parse(s.Trim()))
+                .ToArray());
+            var result = "";
+            foreach (var item in array)
+                result += $"{item}, ";
+            return $" [ {result.Trim(new char[] { ' ', ',' })} ] ";
+        }
+
+        public static int[] GetMaximumSubarray(int[] array)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            if (array.Length <= 1) return array;
+
+            var max = int.MinValue;
+            var subarray = (start: -1, end: -1, total: int.MinValue);
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max) max = array[i];
+                if (array[i] <= 0) continue;
+                for (int j = i; j < array.Length; j++)
+                {
+                    var total = 0;
+                    for (int k = i; k <= j; k++)
+                        total += array[k];
+                    if (total > subarray.total)
+                        subarray = (i, j, total);
+                    else if (total == subarray.total && (j - i) < (subarray.end - subarray.start))
+                        subarray = (i, j, total);
+                }
+            }
+            if (subarray.start >= 0 && subarray.end >= 0)
+            {
+                var result = new int[subarray.end];
+                var index = 0;
+                for (int i = subarray.start; i <= subarray.end; i++)
+                    result[index++] = array[i];
+                Array.Resize(ref result, index);
+                return result;
+            }
+            return new int[] { max };
+        }
+    }
+}
---
+++ src/Exercise-002/MaximumSubarray/MaximumSubarray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MaximumSubarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum Subarray Problem");
+            Console.WriteLine("Sample Array: 1, -3, 2, 1, -1");
+            Console.WriteLine("Quit: q");
+            while (true)
+            {
+                Console.Write("Array: ");
+                var array = Console.ReadLine().Trim();
+                if (string.IsNullOrEmpty(array)) continue;
+                if (array.ToLowerInvariant() == "q") return;
+                var subarray = "";
+                try
+                {
+                    subarray = Calc.GetMaximumSubarray(array);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"ERROR: {e.Message}\r\n");
+                    continue;
+                }
+                Console.WriteLine($"Maximum Subarray: {subarray}\r\n");
+            }
+        }
+    }
+}
