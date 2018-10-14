336ca0ca28932f13f573b4564d749b3d96a79adb
Ahmet U <cyberpunk2099@outlook.com>
2018-10-15 02:47:45 +0300

    refactor

--- src/Exercise-001/FbInterviewQuestion_MsgDecode/FbInterviewQuestion_MsgDecode/Calc.cs
+++ src/Exercise-001/FbInterviewQuestion_MsgDecode/FbInterviewQuestion_MsgDecode/Calc.cs
@@ -7,14 +7,9 @@ namespace FbInterviewQuestion_MsgDecode
 {
     public static class Calc
     {
-        /// <summary>
-        /// Mapping
-        /// </summary>
-        static readonly string[] Table =
-            { "1", "2", "3", "4", "5", "6", "7", "8", "9",
-              "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
-              "20", "21", "22", "23", "24", "25", "26"
-            };
+        static readonly string[] Map = {
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14",
+            "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26" };
 
         public static BigInteger GetDecodeCount(string s, Dictionary<int, BigInteger> memo)
         {
@@ -25,22 +20,23 @@ namespace FbInterviewQuestion_MsgDecode
             switch (len)
             {
                 case 1:
-                    if (Table.Contains(s)) result++;
+                    if (Map.Contains(s)) result++;
                     break;
                 case 2:
-                    if (Table.Contains(s))
+                    if (Map.Contains(s))
                         result++;
-                    if (Table.Contains(s.Substring(0, 1)) && Table.Contains(s.Substring(1, 1)))
+                    if (Map.Contains(s.Substring(0, 1)) && Map.Contains(s.Substring(1, 1)))
                         result++;
                     break;
                 default:
-                    if (Table.Contains(s.Substring(0, 1)))
+                    if (Map.Contains(s.Substring(0, 1)))
                         result = GetDecodeCount(s.Substring(1), memo);
-                    if (Table.Contains(s.Substring(0, 2)))
+                    if (Map.Contains(s.Substring(0, 2)))
                         result += GetDecodeCount(s.Substring(2), memo);
                     break;
             }
-            if (result == 0) throw new FormatException($"Cannot decode '{s}'");
+            if (result == 0 && s.Any(i => !char.IsDigit(i)))
+                throw new ArgumentException($"Cannot decode '{s}'");
             memo.Add(len, result);
             return result;
         }
