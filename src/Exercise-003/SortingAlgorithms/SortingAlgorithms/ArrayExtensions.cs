20b99e9bde396c79610eee82125cdac1d128a58c
Ahmet U <cyberpunk2099@outlook.com>
2018-10-17 00:35:45 +0300

    selection sort added

---
+++ src/Exercise-003/SortingAlgorithms/SortingAlgorithms/ArrayExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    public static class ArrayExtensions
+    {
+        public static void SelectionSort<T>(this T[] a) where T : IComparable<T>
+        {
+            if (a == null || a.Length <= 1) return;
+            var p = 0;
+            while (p < a.Length - 1)
+            {
+                var min = p;
+                for (int i = p + 1; i < a.Length; i++)
+                {
+                    if (a[i].CompareTo(a[min]) < 0)
+                        min = i;
+                }
+                if (a[min].CompareTo(a[p]) < 0)
+                    (a[min], a[p]) = (a[p], a[min]);
+                p++;
+            }
+        }
+
+        public static string ToShortText<T>(this T[] a)
+        {
+            if (a == null) return string.Empty;
+            switch (a.Length)
+            {
+                case 0:
+                    return "[]";
+                case 1:
+                    return $"[{a[0]}]";
+                case 2:
+                    return $"[{a[0]}, {a[1]}]";
+                case 3:
+                    return $"[{a[0]}, {a[1]}, {a[2]}]";
+                case 4:
+                    return $"[{a[0]}, {a[1]}, {a[2]}, {a[3]}]";
+                default:
+                    if (a.Length < 10)
+                        return $"[{a[0]}, {a[1]}, {a[2]},..., {a[a.Length - 1]}]";
+                    return $"[{a[0]}, {a[1]}, {a[2]},..., {a[a.Length / 2]},..., {a[a.Length - 1]}]";
+            }
+        }
+
+        public static int[] CreateRandom(int size, int? max = null, int? min = null)
+        {
+            if (size < 0) throw new ArgumentOutOfRangeException(nameof(size));
+            var result = new int[size];
+            var random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                if (max.HasValue && min.HasValue)
+                    result[i] = random.Next(min.Value, max.Value);
+                else if (max.HasValue)
+                    result[i] = random.Next(max.Value);
+                else
+                    result[i] = random.Next();
+            }
+            return result;
+        }
+    }
+}
---
+++ src/Exercise-003/SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void print(string s) => Console.WriteLine(s);
+Start:
+            Console.WriteLine("\r\nSORT ALGORITHMS\r\n");
+            while (true)
+            {
+                Console.Write("Count: ");
+                if (!int.TryParse(Console.ReadLine(), out var count)) continue;
+                if (count < 4)
+                {
+                    print("Too small to sort!");
+                    continue;
+                }
+                try
+                {
+                    while (true)
+                    {
+                        var array = ArrayExtensions.CreateRandom(count, 100, 1);
+                        print(array.ToShortText());
+                        print($"Sort {count} items with: ");
+                        print(" (S)election Sort");
+                        print(" (B)ubble Sort");
+                        print(" (M)erge Sort");
+                        print("  -or-");
+                        print(" (R)estart");
+                        print(" E(X)IT");
+                        var key = Console.ReadKey().KeyChar.ToString().ToUpper();
+                        var start = Environment.TickCount;
+                        switch (key)
+                        {
+                            case "S":
+                                array.SelectionSort();
+                                print($"\r\n{array.ToShortText()}");
+                                print(
+                                    $"\r\n{count} items are sorted by 'Selection Sort' " +
+                                    $"({Environment.TickCount - start}ms) \r\n");
+                                break;
+                            case "B":
+                                throw new NotImplementedException("B");
+                            case "M":
+                                throw new NotImplementedException("M");
+                            case "R":
+                                goto Start; // :) 
+                            case "X":
+                                return;
+                            default:
+                                print("Select an algorithm");
+                                continue;
+                        }
+                        print(" -press any key to continue- \r\n");
+                        Console.ReadKey();
+                    }
+                }
+                catch (Exception e)
+                {
+                    print($"\r\nERROR: {e.Message}  ({e.GetType().Name})\r\n");
+                    goto Start;
+                }
+            }
+        }
+    }
+}
